4a69e2311bd4a651a544502b909204f77b9ed23d
Samuel Nunes <samuelnunes4039@gmail.com>
2023-12-15 18:21:47 -0300

    Desafio feito

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -25,6 +25,56 @@ class UserController implements Crud {
     return response.status(httpStatus).send(values);
   };
 
+   // função que filtra usuários com idade inferior à idade fornecida no get
+   getById = async (request: Request, response: Response) => {
+    const { age } = request.params;
+    const { httpStatus, values } = await this.citi.getAll();
+
+    const valueToReturn = values.filter(value => value.age < Number(age));
+
+    if (valueToReturn.length === 0) return response.status(httpStatus).send('Não há pessoas usuárias para o filtro escolhido');
+
+    return response.status(httpStatus).send(valueToReturn);
+  };
+
+  // função que calcula e retorna a média de idade dos usuários
+  averageAge = async (request: Request, response: Response) => {
+    const { httpStatus, values } = await this.citi.getAll();
+
+    const ageSum = values.reduce((accumulate, value) => {
+      return accumulate + value.age
+    }, 0);
+
+    const numberOfPeople = values.length
+
+    const average = ageSum / numberOfPeople;
+
+    return response.status(httpStatus).send({ averageAge: average });
+  };
+
+  // Funcao que determina o numero de usuarios idosos no banco de dados
+  OlderUsers = async (request:Request, response: Response) => {
+    const {age} = request.params;
+    const {httpStatus, values} = await this.citi.getAll();
+
+    const OldPeople = values.filter(value => value.age >= 65)
+    if(OldPeople.length === 0) return response.status(httpStatus).send("Não há usuários idosos no banco de dados.");
+
+    return response.status(httpStatus).send(`Existem ${OldPeople.length} idosos no banco de dados`)
+  }
+
+  // Essa funcao retorna o numero de pessoas que possuem o mesmo nome
+  SameNamePeople =async (request: Request, response: Response) => {
+    const {name} = request.params;
+    const {httpStatus, values} = await this.citi.getAll();
+    
+    const namesFilter = values.filter(value => value.firstName === name);
+    
+    if(namesFilter.length === 0) return response.status(httpStatus).send("Não existem pessoas com esse nome no banco de dados.");
+    else if(namesFilter.length === 0) return response.status(httpStatus).send("Existe apenas 1 pessoa com esse nome no banco de dados.");
+    return response.status(httpStatus).send(`Existem ${namesFilter.length} pessoas com esse nome no banco de dados`);
+  }  
+
   delete = async (request: Request, response: Response) => {
     const { id } = request.params;
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,10 @@ const routes = express.Router();
 
 routes.post("/user", userController.create);
 routes.get("/user", userController.get);
+routes.get("/user/:age", userController.getById);  // rota para filtrar usuários por idade
+routes.get("/average", userController.averageAge); // rota para filtrar a média de idade dos usuários
+routes.get("/user/:age", userController.OlderUsers); // Rota para filtrar o número de usuários idosos
+routes.get("/user/:firstName", userController.SameNamePeople); // Rota para filtrar o número de usuários idosos
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
 
